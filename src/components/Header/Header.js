dead8ca1ff76e7ebb1455f3a57329fc4b79a7a71
JorgeIsaacVilla <jvilla10@outlook.es>
2024-01-23 20:54:13 -0500

    Corrección de url a repositorio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ function Header() {
                 <div className="header-access">
                     <div className="logo-restaurante"></div>
                     <div>
-                        <a href="https://eldesviorestobar59.com" className="access-online-tarjet"><strong>Ver carta online</strong></a>
+                        <a href="https://jorgeisaacvilla.github.io/el-desvio-restobar/" className="access-online-tarjet"><strong>Ver carta online</strong></a>
                     </div>
                     <div className="logo-postobon"></div>
                 </div>
